91c3f17d57aaf32942c944c590bb01ae84fc1ae5
Gillian Tunney <gillian@whoisstudio.com>
2019-05-21 15:57:19 -0400

    style updates

--- src/scripts/templates/collection.js
+++ src/scripts/templates/collection.js
@@ -1,3 +1,10 @@
 //import {mountVue} from "../../vue/mountVue";
 //import Catalog from "../../vue/components/shopify/catalog/Catalog.vue";
 //mountVue('#owool-catalog-app', Catalog);
+//import "../../styles/slate/template/collection.scss";
+import "../../styles/slate/templates/collection.scss";
+
+
+//import "../../styles/slate/layout/theme.scss";
+
+
---
+++ src/scripts/templates/product.js
@@ -0,0 +1,1 @@
+import "../../styles/slate/templates/product.scss";
