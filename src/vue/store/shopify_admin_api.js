36980a17be35f302dd3e326ff1b76ca4b77f09ff
Gillian Tunney <gillian@whoisstudio.com>
2019-06-13 17:20:32 -0400

    got rid of pain in the ass submodule

--- src/vue/store/shopify_admin_api.js
+++ src/vue/store/shopify_admin_api.js
@@ -8,10 +8,14 @@ import {Slugify,GDatamapper} from '@/helpers/main.js'
 
 //https://fcf957358a8d22ec7a238b8921cd8c09:427460c7bca64340eefc99d4a71c5d8b@o-wool.myshopify.com/admin/products.json
 
-let BASE_URL = (window.location.hostname == "o-wool-stage.myshopify.com") ? "https://2f1979b64fd471f8692c920838ab735a:e6b8f159238f02584327577ca8ec1a2e@o-wool-stage.myshopify.com/admin/" : "https://o-wool.com"
+let BASE_URL_STAGE =  "https://2f1979b64fd471f8692c920838ab735a:e6b8f159238f02584327577ca8ec1a2e@o-wool-stage.myshopify.com/admin/";
+
+//let BASE_URL_OWOOL = "https://fcf957358a8d22ec7a238b8921cd8c09:427460c7bca64340eefc99d4a71c5d8b@o-wool.myshopify.com/admin/";
+    
+    //(window.location.hostname == "o-wool-stage.myshopify.com") ? "https://2f1979b64fd471f8692c920838ab735a:e6b8f159238f02584327577ca8ec1a2e@o-wool-stage.myshopify.com/admin/" : "https://o-wool.com"
 
 const SHOPIFY_API = new ShopifyApi({
-	baseURL: BASE_URL,
+	baseURL: BASE_URL_STAGE,
 	state: {
         _product:[],
         _shop: false
